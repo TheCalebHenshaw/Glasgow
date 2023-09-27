a04670ad479b21bda49017797e8e829277c1a34d
Caleb Henshaw <145933440+TheCalebHenshaw@users.noreply.github.com>
2023-09-27 14:57:57 +0100

    Update printPrimes.java

--- printPrimes.java
+++ printPrimes.java
@@ -1,1 +1,13 @@
-
+static void printPrimes(int max){
+        for(int i = 2; i<=max;i++){
+            boolean Prime = true;
+            for(int f = 2; f<=i/2;f++){
+                if(i%f==0){
+                    Prime = false;
+                }
+            }
+            if(Prime){
+                System.out.println(i);
+            }
+        }
+    }
